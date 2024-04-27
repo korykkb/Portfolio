e71e5841f2e709d5628d11e438d6fc8234dce6d1
korykkb <korysb@hawaii.edu>
2024-04-26 15:50:57 -1000

    Add collapsible sidebar
    When page is 800px of below, header will turn into a toggle-able sidebar

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,37 @@
 
 //scroll to blur top bar
 window.addEventListener('scroll', function() {
-    var header = document.getElementById('header');
-    if (window.scrollY > 0) {
-      header.classList.add('blur');
-    } else {
-      header.classList.remove('blur');
-    }
-  });
+  const header = document.querySelector('.header');
+  if (window.scrollY > 0) {
+    header.classList.add('blur');
+  } else {
+    header.classList.remove('blur');
+  }
+});
+/*
+window.addEventListener('scroll', function() {
+  const sidebar = document.querySelector('.sidebar');
+  if (window.scrollY > 0) {
+    sidebar.classList.add('blur');
+  } else {
+    sidebar.classList.remove('blur');
+  }
+});  
+*/
   
+//click bars to open sidebar  
+const barsIcon = document.querySelector('.js-bars');
+
+barsIcon.addEventListener('click', function() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex'
+  });
+
+//click x to close sidebar
+
+const xIcon = document.querySelector('.js-x');
+
+xIcon.addEventListener('click', function() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none'
+  });
