661c6973dc7dbfc19010a1595a62f67e5ef7d7d3
korykkb <korysb@hawaii.edu>
2024-05-02 17:30:51 -1000

    Add animations
    Added hover animation for tech stack icons and fade-in animation when scrolling the page

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,17 @@
+//scroll animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
 
 //scroll to blur top bar
 window.addEventListener('scroll', function() {
